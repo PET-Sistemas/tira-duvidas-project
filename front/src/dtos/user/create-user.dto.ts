35e8c7b83289fd0c7ebc01004ef7d5f09d4aec84
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-12-19 01:43:42 -0400

    fix: dados certos de user

--- front/src/dtos/user/create-user.dto.ts
+++ front/src/dtos/user/create-user.dto.ts
@@ -1,7 +1,10 @@
 export class CreateUserDTO {
     email: string
     password: string
+    provider: string
     firstName: string
     lastName: string
     phone: string
+    role: string
+    status: string
 }
