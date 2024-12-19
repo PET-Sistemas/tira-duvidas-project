e59066d3a40e8121680d90be0e60493e0756f2e3
Miguel Albuquerque <miguel8795g@gmail.com>
2024-12-18 21:28:53 -0400

    Feat: Requisições crud de question Services

--- front/src/services/question.service.ts
+++ front/src/services/question.service.ts
@@ -1,7 +1,30 @@
+import { CreateQuestionDTO } from "../dtos/question/create-question.dto";
 
+export function createQuestion(data: CreateQuestionDTO) {
+    return fetch('http://localhost:4001/api/question/', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+}
 
-export async function allQuestions() {
-    const response = await fetch('http://localhost:4001/api/question', { method: 'GET' });
-    const questions = await response.json();
-    return questions;
+export async function allQuestion() {
+    const response = await fetch('http://localhost:4001/api/question/', { method: 'GET' });
+    const users = await response.json();
+    return users;
+}
+
+export async function getQuestion(id:string) {
+    const response = await fetch(`http://localhost:4001/api/question/${id}`, { method: 'GET' });
+    const users = await response.json();
+    return users;
+}
+
+export async function deleteQuestion(id:string) {
+    const response = await fetch(`http://localhost:4001/api/question/${id}`, { method: 'DELETE' });
+    const users = await response.json();
+    return users;
 }
