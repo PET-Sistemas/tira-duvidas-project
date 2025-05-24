ef5d3e1c8c31b60547395829b15d6bb5f3f2bbcf
JoaoP3dr00 <rjoao.pedro05@gmail.com>
2025-05-24 13:12:29 -0400

    feat: adicionando funcionalidade aos botões dos paineis

--- front/src/components/PainelQuestionador/PainelQuestionador.js
+++ front/src/components/PainelQuestionador/PainelQuestionador.js
@@ -2,23 +2,32 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './PainelQuestionador.css';
 import tiraDuvidasLogo from '../Logo-Tira-Dúvidas-removebg.png';
+import { useNavigate } from 'react-router-dom';
 import defaultProfilePic from '../default-profile.png'; // Imagem padrão
 
 function PainelQuestionador() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Remove o username e o id do sessionStorage, redefine o estado e redireciona para a tela de login
+        sessionStorage.removeItem('username');
+        sessionStorage.removeItem('id');
+        navigate('/login');
+    };
+    
     return (
         <div className="container-questionador">
             <header className="perfil-questionador-header">
                 <img src={tiraDuvidasLogo} alt="Tira Dúvidas Logo" className="logo-painel-respondente" />
-                <h2 className="painel-questionador-title">Painel do Usuário</h2>
+                <h2 className="painel-questionador-title">Painel do Questionador</h2>
             </header>
 
             <div className="painel-questionador-card">
                 <ul className="painel-questionador-lista">
-                    <li className="painel-questionador-item">Minhas Dúvidas</li>
-                    <li className="painel-questionador-item">Escrever Nova Dúvida</li>
-                    <li className="painel-questionador-item">Meus Dados Pessoais</li>
-                    
-                    <button className="painel-questionador-sair">Sair</button>
+                    <li className="painel-questionador-item"><button onClick={() => navigate(`/minhas-duvidas`)}>Minhas Dúvidas</button></li>
+                    <li className="painel-questionador-item"><button onClick={() => navigate(`/cadastroduvidas#`)}>Escrever Nova Dúvida</button></li>
+                    <li className="painel-questionador-item"><button onClick={() => navigate(`/perfil`)}>Meus Dados Pessoais</button></li>
+                    <button className="painel-questionador-sair" onClick={handleLogout}>Sair</button>
                 </ul>
             </div>
         </div>
--- front/src/components/PainelRespondente/PainelRespondente.js
+++ front/src/components/PainelRespondente/PainelRespondente.js
@@ -2,24 +2,34 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './PainelRespondente.css';
 import tiraDuvidasLogo from '../Logo-Tira-Dúvidas-removebg.png';
+import { useNavigate } from 'react-router-dom';
 import defaultProfilePic from '../default-profile.png'; // Imagem padrão
 
 function PainelRespondente() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Remove o username e o id do sessionStorage, redefine o estado e redireciona para a tela de login
+        sessionStorage.removeItem('username');
+        sessionStorage.removeItem('id');
+        navigate('/login');
+    };
+
     return (
         <div className="container-respondente">
             <header className="perfil-respondente-header">
                 <img src={tiraDuvidasLogo} alt="Tira Dúvidas Logo" className="logo-painel-respondente" />
-                <h2 className="painel-respondente-title">Painel do Usuário</h2>
+                <h2 className="painel-respondente-title">Painel do Respondente</h2>
             </header>
 
             <div className="painel-respondente-card">
                 <ul className="painel-respondente-lista">
                     <li className="painel-respondente-item">Dúvidas Respondidas</li>
-                    <li className="painel-respondente-item">Responder Dúvidas</li>
-                    <li className="painel-respondente-item">Meus Dados Pessoais</li>
-                    <li className="painel-respondente-item">Minhas Preferências</li>
-                    <li className="painel-respondente-item">Auditar Respostas</li>
-                    <button className="painel-respondente-sair">Sair</button>
+                    <li className="painel-respondente-item"><button onClick={() => navigate(`/responder-duvidas`)}>Responder Dúvidas</button></li>
+                    <li className="painel-respondente-item"><button onClick={() => navigate(`/perfil`)}>Meus Dados Pessoais</button></li>
+                    {/* <li className="painel-respondente-item">Minhas Preferências</li>
+                    <li className="painel-respondente-item">Auditar Respostas</li> */}
+                    <button className="painel-respondente-sair" onClick={handleLogout}>Sair</button>
                 </ul>
             </div>
         </div>
